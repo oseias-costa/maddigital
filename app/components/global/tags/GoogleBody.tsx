1d5e4c24c7fcb7b42558e3bdb3169da058a9f6d6
Oséias Costa <oseiasc2@gmail.com>
2023-10-14 18:40:14 +0000

    feat(tags): create tags folder

---
+++ app/components/global/tags/GoogleBody.tsx
@@ -0,0 +1,15 @@
+"use client;";
+import React from "react";
+
+export default function GoogleBody() {
+  return (
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-ND6CF7K"
+        height="0"
+        width="0"
+        className="display: none; visibility:hidden"
+      ></iframe>
+    </noscript>
+  );
+}
